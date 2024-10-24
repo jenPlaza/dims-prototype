3162a496b7cc58a9c1991c66500976881b3ec62f
Jennifer Plaza <jpplaza@student.fullsail.edu>
2024-10-23 21:28:47 -0400

    Added aypad header to all html pages; created partners.html and logo.css

--- js/dimEdit.js
+++ js/dimEdit.js
@@ -1,24 +1,34 @@
-//NAVIGATION MENU
-var navMenuArray = [
+//AYPAD NAVIGATION MENU
+var navigationArray = [
   {
-    link_id: 'home',
-    link_name: 'Home',
-    link_href: './index.html#DIM',
+    link_id: 'brands',
+    link_name: 'Brands',
+    link_href: '',
   },
   {
     link_id: 'about',
-    link_name: 'About Us',
-    link_href: './about-us.html',
+    link_name: 'About',
+    link_href: 'https://aypad.org/donate#about',
   },
   {
-    link_id: 'service',
-    link_name: 'Our Service',
-    link_href: './our-services.html',
+    link_id: 'story',
+    link_name: 'Story',
+    link_href: 'https://aypad.org/story',
   },
   {
-    link_id: 'team',
-    link_name: 'Our Team',
-    link_href: './our-team.html"',
+    link_id: 'news',
+    link_name: 'News',
+    link_href: 'https://aypad.org/news',
+  },
+  {
+    link_id: 'partners',
+    link_name: 'Partners',
+    link_href: './partners.html',
+  },
+  {
+    link_id: 'donate',
+    link_name: 'Donate',
+    link_href: 'https://aypad.org/donate',
   },
 ];
 
@@ -164,45 +174,168 @@ var testimonialArray = [
   },
 ];
 
+//partners
+var brandArray = [
+  {
+    link_href: 'https://aypad.org',
+    img_src: './images/logos/logo_aypad-min.png',
+    img_alt: 'AYPAD logo',
+    img_caption: 'Africa Youth for Peace and Development',
+  },
+  {
+    link_href: 'https://www.fisori.org/',
+    img_src: './images/partners/freya_logo-min.png',
+    img_alt: 'Freya logo',
+    img_caption: 'Freya',
+  },
+  {
+    link_href: 'https://www.truetalknews.org/',
+    img_src: './images/partners/trueTalk_logo-min.png',
+    img_alt: 'True talk logo',
+    img_caption: 'True Talk',
+  },
+  {
+    link_href: 'https://dimstudio.neocities.org',
+    img_src: './images/logos/logo_DIMS_purple-min.png',
+    img_alt: 'DIM Studio logo',
+    img_caption: 'DIM Studio',
+  },
+];
+//partners
+var logoArray = [
+  {
+    link_href: 'https://www.facebook.com/CommunityYouthNetworkProgramcynpInc/',
+    img_src: './images/partners/communityYouthNetworkProgram.png',
+    img_alt: 'Community Youth Network Program logo',
+    img_caption: 'Community Youth Network Program',
+  },
+  {
+    link_href: 'https://gyan.tigweb.org/',
+    img_src: './images/partners/gyan.png',
+    img_alt: 'Global Youth Action Network logo',
+    img_caption: 'Global Youth Action Network Program',
+  },
+  {
+    link_href: 'https://bitcoin.org/en/',
+    img_src: './images/partners/bitcoin.png',
+    img_alt: 'Bitcoin logo',
+    img_caption: 'Bitcoin',
+  },
+  {
+    link_href: 'https://tools4change.org/',
+    img_src: './images/partners/tools4Change-Logo.png',
+    img_alt: 'Tools 4 Change logo',
+    img_caption: 'Tools 4 Change',
+  },
+  {
+    link_href: 'https://www.warchild.net/',
+    img_src: './images/partners/war-child-ic.png',
+    img_alt: 'War Child logo',
+    img_caption: 'War Child',
+  },
+  {
+    link_href: 'http://yppdatwork.org/',
+    img_src:
+      './images/partners/youthPartnershipForPeaceAndDevelopment_logo.png',
+    img_alt: 'Youth Partnership for Peace and Development logo',
+    img_caption: 'Youth Partnership for Peace and Development',
+  },
+  {
+    link_href: 'https://www.un.org/en/',
+    img_src: './images/partners/uniteNations_logo.png',
+    img_alt: 'United Nations logo',
+    img_caption: 'United Nations',
+  },
+  {
+    link_href: 'https://www.unv.org/',
+    img_src: './images/partners/UNV.jpeg',
+    img_alt: 'United Nations Volunteers logo',
+    img_caption: 'United Nations Volunteers',
+  },
+  {
+    link_href: 'https://www.lapo-nigeria.org/',
+    img_src: './images/partners/LAPO_Logo_2022.png',
+    img_alt: 'Lapo Nigeria logo',
+    img_caption: 'Lapo Nigeria',
+  },
+  {
+    link_href: 'https://www.ecobank.com/personal-banking',
+    img_src: './images/partners/ecobank.png',
+    img_alt: 'Eco Bank logo',
+    img_caption: 'Eco Bank ',
+  },
+  {
+    link_href: 'https://www.betatmercury.com/',
+    img_src: './images/partners/logo_main.png',
+    img_alt: 'Mercury International  logo',
+    img_caption: 'Mercury International ',
+  },
+  {
+    link_href: 'https://www.rokelbank.sl/',
+    img_src: './images/partners/Rokel Bank SL.png',
+    img_alt: 'Rokel Bank logo',
+    img_caption: 'Rokel Bank',
+  },
+];
+
 //FOOTER Social Media
 var socialArray = [
   {
     media_id: 'email',
-    media_link: 'mailto:aypadorg@gmail.com',
+    media_link: [
+      'mailto:publicity.dimstudio@gmail.com',
+      'mailto:aypadorg@gmail.com',
+    ],
     media_src: './images/icons/socialMedia/message_white.png',
     media_alt: 'email icon',
   },
   {
     media_id: 'phone',
-    media_link: 'tel:1-232-75-769929',
+    media_link: ['tel:1-232-75-769929', 'tel:1-232-75-769929'],
     media_src: './images/icons/socialMedia/phone_white.png',
     media_alt: 'phone icon',
   },
   {
     media_id: 'linkedIn',
-    media_link:
+    media_link: [
+      'https://www.linkedin.com/in/dim-studio-ba3531332/',
       'https://www.linkedin.com/company/africa-youth-for-peace-and-development/',
+    ],
     media_src: './images/icons/socialMedia/linkedin_white.png',
     media_alt: 'linkedIn icon',
   },
   {
     media_id: 'facebook',
-    media_link:
+    media_link: [
+      'https://web.facebook.com/profile.php?id=61566708072048',
       'https://www.facebook.com/people/Africa-Youth-for-Peace-and-DevelopmentAYPAD/100065081730525/',
+    ],
     media_src: './images/icons/socialMedia/facebook_white.png',
     media_alt: 'facebook icon',
   },
   {
     media_id: 'twitter',
-    media_link: 'https://x.com/aypadorg1',
+    media_link: [
+      'https://x.com/DimStudio47673?t=4lHHrWqU_8LLuUgI3olhTQ&s=09',
+      'https://x.com/aypadorg1',
+    ],
     media_src: './images/icons/socialMedia/twitter_white.png',
     media_alt: 'twitter icon',
   },
   {
     media_id: 'instgram',
-    media_link: 'https://www.instagram.com/aypadorganization/',
+    media_link: [
+      'https://www.instagram.com/dims_tudio/',
+      'https://www.instagram.com/aypadorganization/',
+    ],
     media_src: './images/icons/socialMedia/instagram_white.png',
     media_alt: 'instgram icon',
+  },
+  {
+    media_id: 'tiktok',
+    media_link: 'https://www.tiktok.com/@dimstudio0',
+    media_src: './images/icons/socialMedia/tiktok_white.png',
+    media_alt: 'tiktok icon',
   },
   {
     media_id: 'youtube',
--- js/dimReplace.js
+++ js/dimReplace.js
@@ -1,12 +1,37 @@
 //HEADER
 const navbar = document.querySelector('.in-place');
+//console.log(window.scrollY);
 (window.onscroll = function () {
-  if (window.scrollY > 125) {
+  if (window.scrollY > 30) {
     navbar.classList.add('scrolling');
   } else {
     navbar.classList.remove('scrolling');
   }
 })();
+
+var aypadHeader = '<div>';
+aypadHeader += '<a href="https://aypad.org/index" id="logo">';
+aypadHeader +=
+  '<figure><img src="./images/logos/wordLogo_bk.png" alt="AYPAD parent company logo"width="100%"height="100%"/></figure></a>';
+aypadHeader +=
+  '<figure id="menu" onclick="appear();"><img src="./images/icons/menu.png" alt="hamburger icon" /></figure>';
+aypadHeader += '<nav id="nav" style="display: none"><ul>';
+aypadHeader +=
+  '<li class="dropdown"><h5 class="brandbtn" onclick="displayBrandList();">Brands</h5></li>';
+for (var i = 1; i < navigationArray.length; i++) {
+  aypadHeader += '<li>';
+  aypadHeader += '<a href="' + navigationArray[i].link_href + '">';
+  aypadHeader += '<h5>' + navigationArray[i].link_name + '</h5></a></li>';
+}
+aypadHeader += '</ul>';
+aypadHeader += '<section class="logo_container" style="display: none">';
+aypadHeader += '<header><div id="close" onclick="closeBranList()">Close</div>';
+aypadHeader += '<h3>Lorem Ipsium</h3>';
+aypadHeader +=
+  '<h5>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and  visual mockups.</h5></header>';
+aypadHeader += '<ul id="brands"></ul>';
+aypadHeader += '</section></nav></div>';
+document.getElementById('AYPAD').innerHTML = aypadHeader;
 
 //NAVIGATION MENU
 //Navigation Menu onclick functionality for mobile sizes
@@ -16,6 +41,23 @@ function appear() {
     navigationList.style.display = 'block';
   } else if (navigationList.style.display == 'block') {
     navigationList.style.display = 'none';
+  }
+}
+//Brand list
+function displayBrandList() {
+  var brandList = document.querySelector('.logo_container');
+  if (brandList.style.display == 'none') {
+    brandList.style.display = 'flex';
+  } else if (brandList.style.display == 'flex') {
+    brandList.style.display = 'none';
+  }
+}
+
+//close Brandlist
+function closeBranList() {
+  var brandList = document.querySelector('.logo_container');
+  if (brandList.style.display == 'flex') {
+    brandList.style.display = 'none';
   }
 }
 
@@ -82,15 +124,50 @@ function displayTestimonials() {
   newEntry += '</ul>';
   document.getElementById('testimonials').innerHTML = newEntry;
 }
+//Partner & Brands
+function displayPartners() {
+  var newLogo = '';
+  for (var t = 0; t < logoArray.length; t++) {
+    newLogo += '<li><article><a href="' + logoArray[t].link_href + '"><figure>';
+    newLogo +=
+      '<img src="' +
+      logoArray[t].img_src +
+      '" alt="' +
+      logoArray[t].img_alt +
+      '" />';
+    newLogo +=
+      '<figcaption><h5>' + logoArray[t].img_caption + '</h5></figcaption>';
+    newLogo += '</figure></a></article></li>';
+  }
+  document.getElementById('logos').innerHTML = newLogo;
+}
+function displayBrands() {
+  var newBrand = '';
+  for (var t = 0; t < brandArray.length; t++) {
+    newBrand +=
+      '<li><article><a href="' + brandArray[t].link_href + '"><figure>';
+    newBrand +=
+      '<img src="' +
+      brandArray[t].img_src +
+      '" alt="' +
+      brandArray[t].img_alt +
+      '" />';
+    newBrand +=
+      '<figcaption><h5>' + brandArray[t].img_caption + '</h5></figcaption>';
+    newBrand += '</figure></a></article></li>';
+  }
+  document.getElementById('brands').innerHTML = newBrand;
+}
+
 //FOOTER
 var mapper = '<ul>';
-for (var k = 1; k < navMenuArray.length; k++) {
+for (var k = 1; k < navigationArray.length; k++) {
   mapper += '<li>';
   mapper +=
     '<a href="' +
-    navMenuArray[k].link_href +
+    navigationArray[k].link_href +
     '"><h6><em>' +
-    navMenuArray[k].link_name +
+    navigationArray[k].link_name +
     '</em></h6></a>';
   mapper += ' </li>';
 }
@@ -98,10 +175,10 @@ mapper += '</ul>';
 document.getElementById('map').innerHTML = mapper;
 
 var media = '<ul>';
-for (var k = 0; k < socialArray.length; k++) {
+for (var k = 0; k < socialArray.length - 1; k++) {
   media += '<li>';
   media +=
-    '<a href="' + socialArray[k].media_link + '" target="_blank"><figure>';
+    '<a href="' + socialArray[k].media_link[0] + '" target="_blank"><figure>';
   media +=
     '<img src="' +
     socialArray[k].media_src +
@@ -111,4 +188,29 @@ for (var k = 0; k < socialArray.length; k++) {
   media += '</li>';
 }
 media += '</ul>';
-document.getElementById('socialM').innerHTML = media;
+document.getElementById('socialM_DIMS').innerHTML = media;
+
+var aypad = '<ul>';
+for (var k = 0; k < socialArray.length - 2; k++) {
+  aypad += '<li>';
+  aypad +=
+    '<a href="' + socialArray[k].media_link[1] + '" target="_blank"><figure>';
+  aypad +=
+    '<img src="' +
+    socialArray[k].media_src +
+    '" alt="' +
+    socialArray[k].media_alt +
+    '" /></figure></a>';
+  aypad += '</li>';
+}
+aypad += '<li>';
+aypad += '<a href="' + socialArray[7].media_link + '" target="_blank"><figure>';
+aypad +=
+  '<img src="' +
+  socialArray[7].media_src +
+  '" alt="' +
+  socialArray[7].media_alt +
+  '" /></figure></a>';
+aypad += '</li>';
+aypad += '</ul>';
+document.getElementById('socialM').innerHTML = aypad;
